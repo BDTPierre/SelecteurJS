2f9bd79fcfd4353fc4db862dca3d0a349b48de45
BDTPierre <buipierre@hotmail.fr>
2024-01-12 10:31:24 +0100

    exo 8/11, fait

--- main.js
+++ main.js
@@ -66,34 +66,49 @@ monlien.setAttribute("href", "https://developer.mozilla.org/fr/docs/Web/JavaScri
 
 // Exercice 7: Utilisation de innerHTML
 // Objectif : Modifier le contenu HTML d'un élément.
-
 // Sélectionnez un élément par son ID.
 // Modifiez son contenu HTML en utilisant innerHTML.
 // Ajoutez des éléments HTML (comme une liste ou un paragraphe) à l'intérieur de cet élément.
 
+let insert = document.getElementById("list");
+insert.innerHTML = "<p>Nouvelle liste!</p>";
+let newlist = document.createElement("ul")
+newlist.innerHTML = "<li>Mangue</li><li>Orange</li><li>Pomme</li><li>Fraise</li>"
+insert.appendChild(newlist);
+
 // Exercice 8: Manipulation de Classes CSS
 // Objectif : Ajouter, enlever et basculer des classes CSS.
-
 // Créez un élément avec une classe CSS pré-définie.
 // Utilisez classList.add pour ajouter une nouvelle classe à cet élément.
 // Utilisez classList.remove pour enlever une classe.
 // Utilisez classList.toggle pour basculer une classe lors d'un événement, comme un clic.
 
+let a = document.getElementById("add");
+let b = a.classList.add("css");
+let c = a.classList.remove("rem");
+
+let d = document.getElementById("button2");
+d.addEventListener("click", function() {
+    a.classList.toggle("rem");
+});
+
+
 // Exercice 9: Manipulation Directe des Styles
 // Objectif : Modifier directement les styles d'un élément.
-
 // Sélectionnez un élément.
 // Modifiez différents styles CSS directement en utilisant element.style.property (par exemple, element.style.backgroundColor).
 
+
+
 // Exercice 10: Travailler avec setAttribute et getAttribute
 // Objectif : Utiliser setAttribute et getAttribute pour manipuler les attributs d'un élément, le "src" d'une image par exemple.
-
 // Sélectionnez un élément (comme un lien ou une image).
 // Utilisez getAttribute pour lire un de ses attributs.
 // Changez cet attribut avec setAttribute.
 
+
+
 // Exercice 11: Création et Manipulation de Listes
 // Objectif : Créer une liste dynamique et la manipuler.
-
 // Créez une liste ul ou ol vide dans votre HTML.
 // Utilisez JavaScript pour ajouter des éléments li à la liste en utilisant appendChild ou insertBefore.
